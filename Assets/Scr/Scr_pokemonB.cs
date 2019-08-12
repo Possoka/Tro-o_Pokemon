5e35e564d45bbabc1be3da6e75538c3b57f1cc99
33328@ETECAF.LOCAL <33328@ETECAF.LOCAL>
2019-08-12 15:29:56 -0300

    Upando o errado

---
+++ Assets/Scr/Scr_pokemonB.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_pokemonB : MonoBehaviour
+{
+    public float BDamage = 0f;    
+    public float BVida = 0f;
+
+    public Src_pokemonA pokeA;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            BVida = BVida - pokeA.ADamage;
+            Debug.Log(BVida.ToString());
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            pokeA.AVida = pokeA.AVida - BDamage;
+            Debug.Log(pokeA.AVida.ToString());
+        }
+
+        if (BVida == 0)
+        {
+            Debug.Log("O Pokemon B morreu");
+        }
+
+        if (pokeA.AVida == 0)
+        {
+            Debug.Log("O pokemon A morreu");
+        }
+    }
+}
---
+++ Assets/Scr/Src_pokemonA.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Src_pokemonA : MonoBehaviour
+{
+
+    public float ADamage = 0f;
+    public float AVida = 0f;
+
+    public Scr_pokemonB pokeB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
